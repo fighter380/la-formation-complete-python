b6b315133cd5566f0344d16a005800efd150ba66
fighter380 <kmillouvalentin@gmail.com>
2025-11-27 17:42:04 +0100

    input exo

--- exr-001_input-utilisateur/01-sources/recuperer_input.py
+++ exr-001_input-utilisateur/01-sources/recuperer_input.py
@@ -1,2 +1,8 @@
 # Demander le prénom, la ville de naissance et l'âge de la personne qui utilise le script
 # Afficher ces trois informations
+prenom = input("veuiller saisir votre nom :")
+ville = input("veuiller saisir votre ville :")
+age = input("veuiller saisir votre age :")
+print(prenom)
+print(ville)
+print(age)
